885fc71d0bae6c25597eed84716812dc9befd0ab
shakil_alam <shakilalam88@gmail.com>
2023-06-18 18:09:44 +0530

    episode 2

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement (It's a feature of some development tools that allows you to see changes to your code in real-time without having to refresh the page or restart the application)
+- File Watching Algorithm - written is c++ (How HMR works)
+- Casing - faster Build
+- Image Optimization
+- Minification 
+- Bundling
+- Compressing
+- Consisting Hashing
+- Code Splitting
+- Differential Bundling -support older browser
+- Diagnostic
+- Error Handling
+- Https
+- Tree shaking -remove unused code
+- Different dev and prod bundle
---
+++ app.js
@@ -0,0 +1,36 @@
+// <div class="container">
+//   <div class="left">
+//     <h1>left h1</h1>
+//     <p>left para</p>
+//   </div>
+//   <div class="right">
+//     <h1>right h1</h1>
+//     <p>right para</p>
+//   </div>
+// </div>
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const div = React.createElement(
+  'div',
+  { className: 'container' },
+  React.createElement(
+    'div',
+    { className: 'left' },
+    React.createElement('h1', { className: 'heading' }, 'left h1'),
+    React.createElement('p', { className: 'para' }, 'left para')
+  ),
+  React.createElement(
+    'div',
+    { className: 'right' },
+    React.createElement('h1', { className: 'heading' }, 'right h1'),
+    React.createElement('p', { className: 'para' }, 'right para')
+  )
+)
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+root.render(div)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(heading)
